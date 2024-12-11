69efe96c13ccbdfbb886d140f188d25ea917b972
matrawy-maxbot <114000998+matrawy-maxbot@users.noreply.github.com>
2024-12-11 23:59:03 +0200

    Update page.js

--- src/app/dashboard/admin/page.js
+++ src/app/dashboard/admin/page.js
@@ -2,7 +2,7 @@
 import dynamic from 'next/dynamic';
 import Head from 'next/head';
 
-const DynamicDashboard = dynamic(() => import('./DashboardContent'), {
+const DynamicDashboard = dynamic(() => import('./pageContent'), {
   ssr: false,
 });
 
