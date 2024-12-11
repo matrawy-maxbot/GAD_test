8f446e7a66446670abf676263cd246b1d4cedea2
matrawy-maxbot <114000998+matrawy-maxbot@users.noreply.github.com>
2024-12-11 22:49:03 +0200

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin();
+ 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
         optimizeCss: true
-    },
-    basePath: '/GAD_test',
-    assetPrefix: '/GAD_test',
+    }
 };
-
-export default nextConfig;
+ 
+export default withNextIntl(nextConfig);
